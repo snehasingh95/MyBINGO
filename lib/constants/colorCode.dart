31511bbcb38e7f9d5fb65a0d5679e56370129fbe
snehasingh95 <sneha121295@gmail.com>
2023-04-16 12:34:59 -0500

    Color Code for App

---
+++ lib/constants/colorCode.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorCode {
+  static const Color BLUE = Color(0xFF0D5FA6);
+  static const Color CYAN = Color(0XFF52CBD9);
+  static const Color LIME_GREEN = Color(0XFFCAD91A);
+  static const Color PEACH = Color(0XFFF8E1D8);
+  static const Color GREY = Color(0XFF3F3F3F);
+}
